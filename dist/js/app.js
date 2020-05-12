0e339cce1fa215b852260c20414f39fc5fdd9c67
Makdoom Shaikh <makshaikh99@gmail.com>
2020-05-12 16:12:02 +0530

    Final Commit for notes taking app 1.0

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,31 +1,44 @@
 // Initialization
-
 const blur = document.getElementById("blur");
 const popup = document.getElementById("popup");
 const addBtn = document.querySelector(".add");
 const noteTitle = document.querySelector(".title");
 const note = document.querySelector(".note");
-const errorMessage = document.querySelector(".error");
 const notesBody = document.querySelector(".notes");
-const msg = document.querySelector(".msg");
+const noteMsg = document.querySelector(".msg");
+const errorMessage = document.querySelector(".error");
+const searchText = document.querySelector("#search-text");
 
+showNotes();
 // Functions
 toggle = (e) => {
   blur.classList.add("active");
   popup.classList.add("active");
 };
+
 addBtn.addEventListener("click", () => {
   if (noteTitle.value !== "" && note.value !== "") {
+    // Getting notes from localStorage
+    let notes = localStorage.getItem("notes");
+
+    // Object of each note
     const data = {
       title: noteTitle.value,
-      note: note.value,
+      desc: note.value,
     };
 
-    // show notes in DOM
-    showNotes(data);
+    if (notes == null) {
+      notesObj = [];
+    } else {
+      notesObj = JSON.parse(notes);
+    }
 
-    // save notes in localStorage
-    localStorage.setItem("Notes", notesBody.innerHTML);
+    notesObj.push(data);
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+    noteTitle.value = "";
+    note.value = "";
+    errorMessage.innerHTML = "";
+    showNotes();
 
     blur.classList.remove("active");
     popup.classList.remove("active");
@@ -34,34 +47,63 @@ addBtn.addEventListener("click", () => {
   }
 });
 
-// checking for notes in localStorage
-let saved = localStorage.getItem("Notes");
-if (saved) {
-  notesBody.innerHTML = saved;
-} else {
-  msg.innerHTML = ` There's Nothing to Show, Please Use "Add Note" Button to Add Notes !!!`;
+function showNotes() {
+  noteMsg.innerHTML = "";
+
+  // Getting notes from localStorage
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+
+  // Note Template
+  let html = "";
+  notesObj.forEach((element, index) => {
+    html += `
+              <div class="note-card" >
+                <div class="note-title"><h3>${element.title}</h3></div>
+                <div class="note-desc"><p>${element.desc}</p></div>
+                <button onclick="deleteNote(${index})" class="remove-btn">Delete</button>
+              </div>
+           `;
+  });
+  if (notesObj.length != 0) {
+    notesBody.innerHTML = html;
+  } else {
+    noteMsg.innerHTML = ` There is Nothing to Show, Please Use "Add Notes" Button to add notes`;
+  }
 }
 
-showNotes = (data) => {
-  notesBody.insertAdjacentHTML(
-    "beforeend",
-    `<div class="note-card" >
-            <div class="note-title">${data.title}</div>
-            <div class="note-desc">
-                ${data.note}
-            </div>
-            <button id="${Math.floor(
-              Math.random() * 1000
-            )}" onclick="deleteNote(this)" class="remove-btn">Delete</button>
-        </div>`
-  );
+// Function to delete a Note
+function deleteNote(index) {
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
 
-  noteTitle.value = "";
-  note.value = "";
-  msg.innerHTML = "";
-  errorMessage.innerHTML = "";
-};
-
-function deleteNote(data) {
-  console.log(typeof JSON.parse(localStorage.getItem("Notes")));
+  notesObj.splice(index, 1);
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+  showNotes();
+  location.reload();
 }
+
+// search event listener
+searchText.addEventListener("input", () => {
+  let inputVal = searchText.value.toLowerCase();
+  console.log(inputVal);
+  let noteCards = document.querySelector(".notes");
+  Array.from(noteCards.children).forEach((element) => {
+    let cardTitle = element.querySelector(".note-title").firstChild.innerText;
+    let cardText = element.querySelector(".note-desc").firstChild.innerText;
+    // checking wheather input is in the card or not
+    if (cardTitle.includes(inputVal) || cardText.includes(inputVal)) {
+      element.style.display = "block";
+    } else {
+      element.style.display = "none";
+    }
+  });
+});
